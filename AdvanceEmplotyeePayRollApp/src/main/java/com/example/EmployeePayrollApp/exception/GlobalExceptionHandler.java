9c446418baaf31e9dfe8d7f90a5eebf676573211
Vardhman Jain <vardhmanjain2003@gmail.com>
2025-03-01 22:19:41 +0530

    [Vardhman Jain] Add. add custom exception created and provide user friendly error

---
+++ AdvanceEmplotyeePayRollApp/src/main/java/com/example/EmployeePayrollApp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.EmployeePayrollApp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    //Handle Validation Errors
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        //Return structured error response
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+}
